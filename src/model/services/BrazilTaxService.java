3c14cd2e42c37d26fdeea852c11d208451525eea
derivaldobrito <derivaldo.brito@yahoo.com.br>
2023-01-31 09:53:57 -0300

    Solution with Interface

--- src/model/services/BrazilTaxService.java
+++ src/model/services/BrazilTaxService.java
@@ -1,14 +1,20 @@
 package model.services;
 
-public class BrazilTaxService {
+public class BrazilTaxService implements TaxService {
 
-    public  Double tax(Double amount){
-        if(amount <= 100.0){
-            return amount * 0.20;
-        }
-        else{
-            return amount * 0.15;
-        }
+    @Override
+    public double tax(double amount) {
+       if(amount <= 100.0){
+        return amount * 0.2;
+       }
+       else{
+
+       }
+        return amount * 0.15;
     }
+
+
+
+
 
 }
